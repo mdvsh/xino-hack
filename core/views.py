c85af74019fd6414be8a4d5f6d17700d1bc81059
Madhav Sharma <shekharmadhav03@gmail.com>
2020-08-12 10:38:19 +0530

    added gitignore

--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Testing.. Hello World</h1>')
+    return HttpResponse('<h1>Testing.. Hello World.</h1> <br> Testing complete..')
