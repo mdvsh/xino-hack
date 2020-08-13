94ec864db8d96b28b84c821392d6f76becea9bdf
Kavin Desi Valli <kavinvalli@gmail.com>
2020-08-13 13:29:41 +0530

    Login Logout registration

--- core/models.py
+++ core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-# from django.contrib.auth.models import User
-from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser
+from django.contrib.auth.models import User
 
 class Places(models.Model):
     name = models.CharField(max_length=100)
@@ -17,35 +16,11 @@ class InterestsActivities(models.Model):
     def __str__(self):
         return self.name
 
-class User(AbstractBaseUser, PermissionsMixin):
-    name = models.CharField(_('Full Name'), max_length=25)
-    email = models.EmailField(_('Email Address'), unique=True)
-    is_applicant = models.BooleanField(_('Applicant'), default=False)
-    is_admin = models.BooleanField(_('Admin'), default=False)
-    is_active = models.BooleanField(_('Active'), default=True)
-    date_joined = models.DateTimeField(_('Date Joined'), auto_now_add=True)
+class CustomUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     isGuide = models.BooleanField(default=False)
     places_of_interest = models.ManyToManyField(Places, related_name='user_places_of_interest')
     interests = models.ManyToManyField(InterestsActivities, related_name='InterestsActivities')
-    objects = UserManager()
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
-
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-
-    def __str__(self):
-        return self.email
-
-    def clean(self):
-        super().clean() 
-        self.email = self.__class__.objects.normalize_email(self.email)
-# class CustomUser(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     isGuide = models.BooleanField(default=False)
-#     places_of_interest = models.ManyToManyField(Places, related_name='user_places_of_interest')
-#     interests = models.ManyToManyField(InterestsActivities, related_name='InterestsActivities')
 
 class Hiring(models.Model):
     traveller = models.ForeignKey(CustomUser, related_name='hiring_traveller', on_delete=models.SET_NULL, null=True)
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import Homepage
-
+from . import views
 
 urlpatterns = [
-    path('', Homepage.as_view(), name='home')
+    path('', Homepage.as_view(), name='home'),
+    path('login/', views.login_function, name='login'),
+    path('registration/', views.register_function, name='registration'),
+    path("activate/<uidb64>/<token>", views.activate_account, name="activate"),
+    path('logout/', views.logout_view, name='logout'),
 ]
